e4cf2fc5fd5d60c4865cdf231f35af7ddccda346
Nekrozu <luqhakimi.rahman@gmail.com>
2026-04-11 01:13:48 +0800

    Added All Starter Deck Cards Images

---
+++ src/data/cards.js
@@ -0,0 +1,17 @@
+import redStartDeck from './sets/26RSD01.json';
+import purpleStartDeck from './sets/26RSD02.json';
+import greenStartDeck from './sets/26RSD03.json';
+import whiteStartDeck from './sets/26RSD04.json';
+import yellowStartDeck from './sets/26RSD05.json';
+import blueStartDeck from './sets/26RSD06.json';
+
+const allCards = [
+  ...redStartDeck, 
+  ...purpleStartDeck,
+  ...greenStartDeck,
+  ...whiteStartDeck,
+  ...yellowStartDeck,
+  ...blueStartDeck
+];
+
+export default allCards;
