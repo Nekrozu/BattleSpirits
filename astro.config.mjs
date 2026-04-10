44cf42c1453fa2fed19586524190526e1a354959
Nekrozu <luqhakimi.rahman@gmail.com>
2026-04-10 14:57:51 +0800

    Added Astro site, Tailwind, and database

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Nekrozu.github.io',
+  base: '/BattleSpirit',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
