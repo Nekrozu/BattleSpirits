6957ca8844874c2fa8f6576597d2c4f204227df7
Nekrozu <luqhakimi.rahman@gmail.com>
2026-06-07 00:47:00 +0800

    Preparation

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,17 +1,37 @@
-import redStartDeck from './sets/26RSD01.json';
-import purpleStartDeck from './sets/26RSD02.json';
-import greenStartDeck from './sets/26RSD03.json';
-import whiteStartDeck from './sets/26RSD04.json';
-import yellowStartDeck from './sets/26RSD05.json';
-import blueStartDeck from './sets/26RSD06.json';
+import wingfangsOfScarletBlaze from './sets/26RSD01.json';
+import bloodrouseOfTheVioletPhantom from './sets/26RSD02.json';
+import armoredFishOfTheEmeraldSea from './sets/26RSD03.json';
+import mineroidOfPaleDestiny from './sets/26RSD04.json';
+import thunderDragonOfTheRadiantSun from './sets/26RSD05.json';
+import ferobeastOfTheAzureAbyss from './sets/26RSD06.json';
+import kamenRider_AgentOfDream from './sets/26RSD07.json';
+import symphonyOfTheBeginning from './sets/26RSD08.json';
+import thePurgatoryPheonixOfAwakening from './sets/26RSD09.json';
+import theAberrantWhaleOfAbyss from './sets/26RSD10.json';
+
+import theGenesisPulse from './sets/26RBS01.json';
+import theIllusoryWinds from './sets/26RBS02.json';
+import theRulerOfZekkai from './sets/26RBS03.json';
+
+
 
 const allCards = [
-  ...redStartDeck, 
-  ...purpleStartDeck,
-  ...greenStartDeck,
-  ...whiteStartDeck,
-  ...yellowStartDeck,
-  ...blueStartDeck
+  ...wingfangsOfScarletBlaze,
+  ...bloodrouseOfTheVioletPhantom,
+  ...armoredFishOfTheEmeraldSea,
+  ...mineroidOfPaleDestiny,
+  ...thunderDragonOfTheRadiantSun,
+  ...ferobeastOfTheAzureAbyss,
+  ...kamenRider_AgentOfDream,
+  ...symphonyOfTheBeginning,
+  ...thePurgatoryPheonixOfAwakening,
+  ...theAberrantWhaleOfAbyss,
+
+  ...theGenesisPulse,
+  ...theIllusoryWinds,
+  ...theRulerOfZekkai
+
+
 ];
 
 export default allCards;
